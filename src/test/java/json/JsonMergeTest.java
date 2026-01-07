90dab37806a362496d79f7d4496f79719f938064
David Strömner <davidstromner@gmail.com>
2026-01-07 18:07:33 +0100

    Updated to Jackson3 (#57)
    Co-authored-by: David Strömner <david.stromner@stralfors.se>
    Co-authored-by: Hemant Patel <hemantsonu20@gmail.com>

--- src/test/java/json/JsonMergeTest.java
+++ src/test/java/json/JsonMergeTest.java
@@ -1,8 +1,8 @@
 package json;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
+import tools.jackson.databind.JsonNode;
+import tools.jackson.databind.ObjectMapper;
+import tools.jackson.databind.node.ArrayNode;
 import com.github.hemantsonu20.json.JsonMerge;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.TestFactory;
